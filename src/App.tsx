5412832ab1eac012a6fc383bda3ededa7d9b580b
QuispeRosasGabriel <gabrielomarquisperosas@gmail.com>
2022-10-21 00:10:49 -0500

    added listener to get enter keydown

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+const dashSymbol = '-';
+const hashtagSymbol = '#';
 
 function App () {
-  
+
 const [textInformation, setTextInformation] = useState<unknown>();
 
-  const handleTextChange = (textValue = '') => {
+useEffect(() => {
+  const keyDownHandler = (event: any) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      setTextInformation(<br />);
+    }
+  };
+  document.addEventListener('keydown', keyDownHandler);
+
+  return () => {
+    document.removeEventListener('keydown', keyDownHandler);
+  };
+}, []);
+
+
+  const handleTextChange = (textValue = '', code = '') => {
     //TODO Find a way to implement second point of the AC (see README for full info)
-    if (!textValue) {
+    console.log('hereee', code)
+    if (textValue.length === 0) {
       setTextInformation(<br />);
       return;
     }
-    if (textValue.split('')[0] === '#' && textValue.split('')[1] !== '#') {
+    if (textValue.split('')[0] === hashtagSymbol && textValue.split('')[1] !== hashtagSymbol) {
       setTextInformation(
         <h1>
           {textValue
             .split('')
-            .filter((t) => t !== '#')
-            .filter((t) => t !== '##')
+            .filter((t) => t !== hashtagSymbol)
+            .filter((t) => t !== `${hashtagSymbol}${hashtagSymbol}`)
             .join('')}
         </h1>,
       );
@@ -24,13 +42,13 @@ const [textInformation, setTextInformation] = useState<unknown>();
       return;
     }
 
-    if (textValue.split('')[0] === '#' && textValue.split('')[1] === '#') {
+    if (textValue.split('')[0] === hashtagSymbol && textValue.split('')[1] === hashtagSymbol) {
       setTextInformation(
         <h2>
           {textValue
             .split('')
-            .filter((t) => t !== '#')
-            .filter((t) => t !== '##')
+            .filter((t) => t !== hashtagSymbol)
+            .filter((t) => t !== `${hashtagSymbol}${hashtagSymbol}`)
             .join('')}
         </h2>,
       );
@@ -38,9 +56,9 @@ const [textInformation, setTextInformation] = useState<unknown>();
     }
 
     if (
-      textValue.split('')[0] === '-' &&
-      textValue.split('')[1] === '-' &&
-      textValue.split('')[2] === '-'
+      textValue.split('')[0] === dashSymbol &&
+      textValue.split('')[1] === dashSymbol &&
+      textValue.split('')[2] === dashSymbol
     ) {
       setTextInformation(<hr />);
       return;
@@ -51,7 +69,7 @@ const [textInformation, setTextInformation] = useState<unknown>();
 
   return (
     <div>
-      <textarea onChange={(e: any) => handleTextChange(e.target.value)} rows={5} />
+      <textarea onChange={(e: any) => handleTextChange(e.target.value, e.key)} rows={5} />
       <>{textInformation}</>
     </div>
   );
