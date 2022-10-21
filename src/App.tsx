34527fa23ac9ddbc631612e0e04a180b5bd8b35c
QuispeRosasGabriel <gabrielomarquisperosas@gmail.com>
2022-10-20 23:09:45 -0500

    WIP markdown react

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,58 @@
-function App() {
+import { useState } from 'react';
+
+function App () {
+  
+const [textInformation, setTextInformation] = useState<unknown>();
+
+  const handleTextChange = (textValue = '') => {
+    //TODO Find a way to implement second point of the AC (see README for full info)
+    if (!textValue) {
+      setTextInformation(<br />);
+      return;
+    }
+    if (textValue.split('')[0] === '#' && textValue.split('')[1] !== '#') {
+      setTextInformation(
+        <h1>
+          {textValue
+            .split('')
+            .filter((t) => t !== '#')
+            .filter((t) => t !== '##')
+            .join('')}
+        </h1>,
+      );
+
+      return;
+    }
+
+    if (textValue.split('')[0] === '#' && textValue.split('')[1] === '#') {
+      setTextInformation(
+        <h2>
+          {textValue
+            .split('')
+            .filter((t) => t !== '#')
+            .filter((t) => t !== '##')
+            .join('')}
+        </h2>,
+      );
+      return;
+    }
+
+    if (
+      textValue.split('')[0] === '-' &&
+      textValue.split('')[1] === '-' &&
+      textValue.split('')[2] === '-'
+    ) {
+      setTextInformation(<hr />);
+      return;
+    }
+
+    return setTextInformation(<p>{textValue}</p>);
+  };
+
   return (
     <div>
-      <textarea onChange={(e) => console.log(e.target.value)} rows={5} />
+      <textarea onChange={(e: any) => handleTextChange(e.target.value)} rows={5} />
+      <>{textInformation}</>
     </div>
   );
 }
